a7e83010216220fe9aca7e424714dc40c3547fd6
molkodmitriy <molko_dmitriy@mail.ru>
2021-02-07 18:26:18 +0700

    Small fix

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 
 const int n = 9;
-
+void OutArea(char (&a)[n][n])
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cout << a[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
 int main()
 {
     using namespace std;
     char ChessArea[n][n];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             ChessArea[i][j] = ' ';
         }
     }
-    for (int i = 0; i < n; i++) 
-    {
-       ChessArea[i][0] =(char)(48+(n-i-1));
-       ChessArea[8][i] = (char)(96 + i);
+    for (int i = 0; i < n; i++) {
+        ChessArea[i][0] = (char)(48 + (n - i - 1));
+        ChessArea[8][i] = (char)(96 + i);
     }
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++) {
         ChessArea[1][i] = 'p';
     }
     ChessArea[0][1] = 'r';
@@ -30,18 +34,11 @@ int main()
     ChessArea[0][6] = 'b';
     ChessArea[0][7] = 'n';
     ChessArea[0][8] = 'r';
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++) {
         ChessArea[7][i] = (char)(ChessArea[0][i] - 32);
         ChessArea[6][i] = (char)(ChessArea[1][i] - 32);
     }
     ChessArea[8][0] = ' ';
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << ChessArea[i][j] << ' ';
-        }
-        cout << endl;
-    }
+
+    OutArea(ChessArea);
 }
